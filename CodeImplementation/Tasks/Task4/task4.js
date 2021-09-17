e6cff92dfb2a2b294196fcc2346ccfe6876b19c7
riyaraa <rinosatyaputra.id@gmail.com>
2021-09-17 13:02:34 +0700

    Done Revisited

--- CodeImplementation/Tasks/Task4/task4.js
+++ CodeImplementation/Tasks/Task4/task4.js
@@ -1,11 +1,13 @@
 function divideAndSort(angka) {
 	const num = angka.toString().split(0);
 	const devideNum = [...num].map((value) => value);
-	// const results = devideNum.join('');
-	const idx1 = devideNum[0].split("").sort().join("");
-	const idx2 = devideNum[1].split("").sort().join("");
-	const idx3 = devideNum[2].split("").sort().join("");
-	const results = `${idx1}${idx2}${idx3}`;
+	// const idx1 = devideNum[0].split("").sort().join("");
+	// const idx2 = devideNum[1].split("").sort().join("");
+	// const idx3 = devideNum[2].split("").sort().join("");
+	// const results = `${idx1}${idx2}${idx3}`;
+	const results = devideNum
+		.map((value) => value.split(" ").sort().join(""))
+		.join("");
 	return results;
 }
 console.log(divideAndSort(5956560159466056));
